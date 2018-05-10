67e1578a653824c39cd7311d47cbc4b3a70609e5
Jeanie  Orejarena <jeanie@crossfield.com>
2018-05-10 16:10:41 -0400

    Adding read more file Adding npm ignore file and updating the package version

---
+++ README.md
@@ -0,0 +1,48 @@
+# Read more
+
+![demo](demo.gif)
+
+```
+npm install @crossfield/react-react-more
+```
+
+This read more component helps truncate large chunks of text/html and animates to the full height of the inner content.
+
+
+# Example
+
+```js
+<Readmore
+  initialHeight={350}
+  readMore={props => (
+  <ReadMoreButton onClick={props.onClick}>
+    {props.open ? 'Read Less' : 'Read More'}
+    <Icon
+    name="caret"
+    style={{
+      transform: `rotate( ${props.open ? '-90deg' : '90deg'})`,
+      transition: 'transform 0.25s',
+    }}
+    />
+  </ReadMoreButton>
+  )}
+>
+  <Label>The Design Vision</Label>
+  <Text>
+      SOME LONG TEXT
+  </Text>
+</Readmore>
+```
+
+# Props
+
+**initialHeight**
+
+The closed size of the container
+
+**readMore**
+
+render function that gives you a props object containing `open` and `onClick` to toggle open / close
+
+
+ 
--- src/index.js
+++ src/index.js
@@ -1,3 +1,66 @@
 import React from 'react';
 
-export default () => <div>test</div>;
+export default class ReadMore extends React.Component {
+  constructor({ initialHeight }) {
+    super();
+    this.state = { initialHeight, maxHeight: initialHeight };
+  }
+
+  toggle() {
+    let { maxHeight, initialHeight } = this.state;
+
+    //if expanded, close it
+    if (maxHeight !== initialHeight)
+      return this.setState({ maxHeight: initialHeight });
+
+    //calculate height of all the children
+    let children = [...this.container.children];
+    let height = 0;
+    children.forEach(child => (height += child.offsetHeight));
+
+    //set the full height
+    this.setState({ maxHeight: height });
+  }
+
+  render() {
+    let { children, readMore } = this.props;
+    let { maxHeight, initialHeight } = this.state;
+    let open = maxHeight !== initialHeight;
+
+    return (
+      <React.Fragment>
+        <div
+          style={{
+            maxHeight,
+            transition: 'max-height .5s ease',
+            position: 'relative',
+            overflow: 'hidden',
+            marginTop: 40,
+          }}
+          ref={el => (this.container = el)}
+        >
+          {children}
+          <div
+            style={{
+              transition: 'opacity 0.25s',
+              opacity: open ? 0 : 1,
+              backgroundImage:
+                'linear-gradient(to bottom, rgba(255, 255, 255, 0.44), #ffffff )',
+              content: '',
+              height: '160px',
+              width: '100%',
+              position: 'absolute',
+              bottom: '0',
+              left: '0',
+            }}
+          />
+        </div>
+
+        {readMore({
+          open,
+          onClick: () => this.toggle(),
+        })}
+      </React.Fragment>
+    );
+  }
+}
